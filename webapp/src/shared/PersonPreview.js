ef9a4546bba8a191796cc70e9f5b9a6906bf4d2b
Renchu Song <songrenchu@Renchus-MacBook-Pro.local>
2021-11-14 18:52:26 -0500

    Fix person preview icon

--- webapp/src/shared/PersonPreview.js
+++ webapp/src/shared/PersonPreview.js
@@ -1,13 +1,24 @@
 
 
 import React from 'react';
-import { Avatar, Button, List } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Image, List } from 'antd';
 
-const PersonPreview = ({name, gender, height, age}) => (
+const PersonPreview = ({ name, gender, height, age }) => (
   <List.Item>
     <List.Item.Meta
-      avatar={<Avatar shape="square" size={48} icon={<UserOutlined />} />}
+      avatar={
+        <Avatar
+          shape="square"
+          size={48}
+          src={
+            <Image
+              src={`${process.env.PUBLIC_URL}/avatar_${gender}.png`}
+              style={{ width: 48, backgroundColor: "#ccc" }}
+              preview={false}
+            />
+          }
+        />
+      }
       title={name}
       description={`${gender}, ${height}cm, ${age} years old`}
     />
