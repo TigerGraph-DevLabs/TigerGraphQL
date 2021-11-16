8d9165d5ceb202d73edb05923ebed88292c315ce
yandaev <91520755+yandaev@users.noreply.github.com>
2021-11-16 18:24:12 +0300

    fixed beautifier for date

--- webapp/src/shared/utility.js
+++ webapp/src/shared/utility.js
@@ -2,6 +2,6 @@ export function beautifyDate(datetime) {
   const month = [
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
   ];
-  const dt = new Date(datetime);
+  const dt = new Date(datetime.split(' ')[0]);
   return `${month[dt.getMonth()]} ${dt.getDate()}, ${dt.getFullYear()}`;
 }
