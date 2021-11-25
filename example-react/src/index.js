16cadcb7d51850972b81f5f0172268464c805b98
lin.yu <lin.yu@tigergraph.com>
2021-11-25 12:43:42 +0800

    add config.json support for apollo

--- example-react/src/index.js
+++ example-react/src/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import client from './apollo';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+function Root() {
+  const [apolloClient, setApolloClient] = useState(null);
+  useEffect(async () => {
+    let config = await (await fetch("/config.json")).json();
+    let apolloClient = new ApolloClient({
+      uri: config["apolloURL"],
+      headers: {
+        "Content-Type": "application/graphql",
+      },
+      cache: new InMemoryCache(),
+    });
+    setApolloClient(apolloClient);
+  }, []);
+
+  if (!apolloClient) {
+    return <div>init...</div>;
+  }
+
+  return (
+    <React.Fragment>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+      ,
+    </React.Fragment>
+  );
+}
 
 ReactDOM.render(
-  <React.Fragment>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-  </React.Fragment>,
+  <Root />,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
